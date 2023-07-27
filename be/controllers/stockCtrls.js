385b7bd9f52f5a401c82240273094b250188f7b8
RamzGxz <ranzdaffa32@gmail.com>
2023-07-28 00:37:12 +0700

    add module

--- be/controllers/stockCtrls.js
+++ be/controllers/stockCtrls.js
@@ -1,22 +1,5 @@
 const stockModels = require('../models/stockModels')
-
-const getIndonesianDate = (date) => {
-    const options = {
-        timeZone: 'Asia/Jakarta',
-        weekday: 'long',
-        year: 'numeric',
-        month: 'long',
-        day: 'numeric',
-        hour: '2-digit',
-        minute: '2-digit',
-        second: '2-digit'
-    }
-
-    return date.toLocaleString('id-ID', options)
-}
-
-
-
+const getIndonesianDate = require('../controllers/timeIndonesianModule')
 
 module.exports = {
     getStock: async (req, res) => {
